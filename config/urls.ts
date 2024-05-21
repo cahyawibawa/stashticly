68f7654d54a579be89d97a25079a6b571331465d
double fantasy <cahyawibawa.dev@gmail.com>
2024-05-21 12:56:54 +0700

    feat: init

---
+++ config/urls.ts
@@ -0,0 +1,24 @@
+export const urls = {
+  name: 'Stashticly',
+  github: 'https://github.com/',
+  features:{
+    path: [
+      // {
+      //   path: "/updates",
+      //   name: "updates",
+      // },
+      {
+        path: "/pricing",
+        name: "pricing",
+      },
+      {
+        path: "/dashboard",
+        name: "dashboard",
+      },
+      {
+        path: "/pitch",
+        name: "pitch",
+      },
+    ],
+  }
+};
