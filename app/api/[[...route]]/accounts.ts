347372bb77252ac7ea1c4a802bda56a6e9b71340
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-05-23 21:27:03 +0700

    fix: delete error handling

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -3,15 +3,12 @@ import { accounts } from '@/db/schema'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { eq } from 'drizzle-orm'
 import { Hono } from 'hono'
-import { HTTPException } from 'hono/http-exception'
 
 const app = new Hono().get('/', clerkMiddleware(), async (c) => {
   const auth = getAuth(c)
 
   if (!auth?.userId) {
-    throw new HTTPException(401, {
-      res: c.json({ error: 'Unauthorized' }, 401),
-    })
+    return c.json({ error: 'Unauthorized' }, 401)
   }
 
   const data = await db
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,19 +1,10 @@
 import { Hono } from 'hono'
-import { HTTPException } from 'hono/http-exception'
 import { handle } from 'hono/vercel'
 import accounts from './accounts'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
-
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse()
-  }
-
-  return c.json({ error: 'Internal error' }, 500)
-})
 
 const routes = app.route('/accounts', accounts)
 
