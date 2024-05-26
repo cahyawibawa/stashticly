2fc06e3275006cf50051d1c4a2dd848deb74a394
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-05-26 18:37:42 +0700

    feat: categories API routes

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,12 +1,13 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import accounts from './accounts'
+import categories from './categories'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-const routes = app.route('/accounts', accounts)
+const routes = app.route('/accounts', accounts).route('/categories', categories)
 
 export const GET = handle(app)
 export const POST = handle(app)
