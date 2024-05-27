76c0b47f800cfc1ef25671c86d0dc442e78f75f2
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-05-27 12:54:55 +0700

    feat: Transaction API

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,12 +2,16 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import accounts from './accounts'
 import categories from './categories'
+import transactions from './transactions'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-const routes = app.route('/accounts', accounts).route('/categories', categories)
+const routes = app
+  .route('/accounts', accounts)
+  .route('/categories', categories)
+  .route('/transactions', transactions)
 
 export const GET = handle(app)
 export const POST = handle(app)
--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,3 @@
-import { on } from 'events'
 import { relations } from 'drizzle-orm'
 import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 import { createInsertSchema } from 'drizzle-zod'
