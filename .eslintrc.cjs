958ed64327dd46195cc08d2c4bfc8a0b321e4d8d
double fantasy <cahyawibawa.dev@gmail.com>
2024-06-05 16:26:16 +0700

    feat: eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,7 @@ const config = {
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'off',
       '@typescript-eslint/consistent-type-imports': [
           'warn',
           {
