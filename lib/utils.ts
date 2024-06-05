fef396e2986655edf423a4d8027523b9ed597b23
double fantasy <cahyawibawa.dev@gmail.com>
2024-06-05 16:21:27 +0700

    feat: utils currency

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function convertAmountFromMiliUnits(amount: number) {
+  return amount / 1000
+}
+
+export function convertAmountToMiliUnits(amount: number) {
+  return Math.round(amount * 1000)
+}
+
+//TODO change currency to Rupiah
+
+export function formatCurrency(value: number){
+  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(value)
+}
