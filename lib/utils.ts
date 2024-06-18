77eaffd5ba9320f1b3b10c5e04496d95d7e0bc44
double fantasy <cahyawibawa.dev@gmail.com>
2024-06-18 22:08:46 +0700

    feat: new format

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
 import { clsx, type ClassValue } from 'clsx'
-import { eachDayOfInterval, isSameDay } from 'date-fns'
+import { eachDayOfInterval, format, isSameDay, subDays } from 'date-fns'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
@@ -15,7 +15,6 @@ export function convertAmountToMiliUnits(amount: number) {
 }
 
 //TODO change currency to Rupiah
-
 export function formatCurrency(value: number) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -62,4 +61,41 @@ export function fillMissingDays(
   })
 
   return transactionsByDay
+}
+
+type Period = {
+  from: string | Date | undefined
+  to: string | Date | undefined
+}
+
+export function formatDateRange(period?: Period) {
+  const defaultTo = new Date()
+  const defaultFrom = subDays(defaultTo, 30)
+
+  if (!period?.from) {
+    return `${format(defaultFrom, 'LLL dd')} - ${format(defaultTo, 'LLL dd, y')}`
+  }
+
+  if (period.to) {
+    return `${format(period.from, 'LLL dd')} - ${format(period.to, 'LLL dd, y')}`
+  }
+
+  return format(period.from, 'LLL dd, y')
+}
+
+export function formatPercentage(
+  value: number,
+  options: { addPrefix?: boolean } = {
+    addPrefix: false,
+  }
+) {
+  const result = new Intl.NumberFormat('en-US', {
+    style: 'percent',
+  }).format(value / 100)
+
+  if (options.addPrefix && value > 0) {
+    return `+${result}`
+  }
+
+  return result
 }
